408b6a84be1541d1411756a467e6a10d84093212
Chen Tao <chandagaz84521@gmail.com>
2024-11-27 09:23:24 +0800

    Interleave String2

--- DP/LCS/interleaving-string/Solution.h
+++ DP/LCS/interleaving-string/Solution.h
@@ -35,4 +35,26 @@ public:
         int a = dfs(dfs,n-1,m-1,l-1);
         return a > 0? true:false;
     }
+
+    bool isInterleave2(string s1, string s2, string s3){
+        int n = s1.size(), m = s2.size(), l = s3.size();
+        if(m+n != l)
+            return false;
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        dp[0][0] = true;
+        for(int i = 1; i <= n; i++){
+            if(s1[i-1] == s3[i-1])
+                dp[i][0] = dp[i-1][0];
+        }
+        for(int i = 1; i <= m; i++){
+            if(s2[i-1] == s3[i-1])
+                dp[0][i] = dp[0][i-1];
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                dp[i][j] = (s1[i-1] == s3[i+j-1] && dp[i-1][j]) || (s2[j-1] == s3[i+j-1] && dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
 };
